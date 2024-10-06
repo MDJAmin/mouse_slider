582d83b62e1934dde9e2243c06b7392d223544e6
MDJAmin <mohammadamindf@gmail.com>
2024-10-06 15:57:39 +0330

    Feat : Get items

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict"
+const track = document.querySelector("#image-track");
+
+const handleOnDown = (e)=>{
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+};
